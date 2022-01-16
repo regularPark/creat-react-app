2276e7238f7007cd65c15486bd965714e0fda6ce
JayPark0110 <90092440+JayPark0110@users.noreply.github.com>
2022-01-16 18:28:26 +0900

    1st css

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,28 +1,40 @@
 import propTypes from "prop-types";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({coverImg, title, rating, genres, runtime,year, summary,id}){
-    return <div>
-    <img src={coverImg} alt={title} />
-    <h2>
+function Movie({
+  coverImg,
+  title,
+  rating,
+  genres,
+  runtime,
+  year,
+  summary,
+  id,
+  movie_style,
+}) {
+  return (
+    <div className={styles.movie} style={movie_style}>
+      <img src={coverImg} alt={title} className={styles.movie_img} />
+      <h2 className={styles.movie_title}>
         <Link to={`/movie/${id}`}>{title}</Link>
-    </h2>
-    <h3>rating: {rating}</h3> 
-    <h3>genres</h3>
-    <p>{summary.length > 235 ? `${summary.slice(0,235)}...`: summary}</p>
-    <ul>
-        {genres && genres.map((g) => <li key={g}> {g} </li>) }
-    </ul>    
-</div>;
+      </h2>
+      <p className={styles.movie_summary}>
+        {summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}
+      </p>
+      <ul className={styles.movie_genres}>
+        {genres && genres.map((g) => <li key={g}> {g} </li>)}
+      </ul>
+    </div>
+  );
 }
 
 Movie.propTypes = {
-    id: propTypes.number.isRequired,
-    coverImg: propTypes.string.isRequired,
-    title: propTypes.string.isRequired,
-    rating: propTypes.number.isRequired,
-    genres: propTypes.arrayOf(propTypes.string),
+  id: propTypes.number.isRequired,
+  coverImg: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+  rating: propTypes.number.isRequired,
+  genres: propTypes.arrayOf(propTypes.string),
 };
-
 
 export default Movie;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,38 +1,48 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
-import Movie from '../components/Movie';
+import { useState } from "react";
+import { useEffect } from "react";
+import Movie from "../components/Movie";
 
-function Home(){
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-        const json = await (
-            await fetch(
-            "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
-            ).json();
-        setMovies(json.data.movies);
-        setLoading(false);        
-    }
-    useEffect(()=>{
-        getMovies();
-    },[]);
-    return (<div>
-        {loading ? (<h1>Loading...</h1>) : (
-        <div>{movies.map((movie => 
-            (<Movie
-                key={movie.id}
-                coverImg={movie.medium_cover_image} 
-                title={movie.title} 
-                genres={movie.genres}
-                rating={movie.rating}
-                runtime={movie.runtime}
-                year={movie.year}
-                summary={movie.summary}
-                id={movie.id}/>)
-            ))}
-        </div>        
-    )}
-    </div>);
+import styles from "./Home.module.css";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className={styles.container}>
+          {movies.map((movie) => (
+            <Movie
+              className="movie"
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              genres={movie.genres}
+              rating={movie.rating}
+              runtime={movie.runtime}
+              year={movie.year}
+              summary={movie.summary}
+              id={movie.id}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Home;
